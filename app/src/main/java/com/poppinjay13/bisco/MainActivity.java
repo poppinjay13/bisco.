f28794c7435eeaa93eea852be618b0d9809dfa06
Ian Odundo <38793943+poppinjay13@users.noreply.github.com>
2020-01-21 18:21:19 +0300

    initial commit
    Basic android empty activity app

---
+++ app/src/main/java/com/poppinjay13/bisco/MainActivity.java
@@ -0,0 +1,14 @@
+package com.poppinjay13.bisco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
